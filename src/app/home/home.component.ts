0687f1cc2e4ec5cab4e058f879eb9c72738f9c09
ryanlerler <lyp2726@outlook.com>
2024-07-06 14:34:59 +0800

    Create interface, add input & property binding to component, add dynamic values to template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { HousingLocation } from "../housing-location";
 
 @Component({
   selector: "app-home",
@@ -20,4 +21,6 @@ import { HousingLocationComponent } from "../housing-location/housing-location.c
   `,
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = []
+}
