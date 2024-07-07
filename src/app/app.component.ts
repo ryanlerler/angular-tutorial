904250476f53b453c77a50300f6e32a58e30ba4b
ryanlerler <lyp2726@outlook.com>
2024-07-07 09:44:05 +0800

    Add route to home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
@@ -15,12 +16,13 @@ import { HomeComponent } from "./home/home.component";
         />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <!-- <<app-home></app-home>> -->
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
   styleUrls: ["./app.component.css"],
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
 })
 export class AppComponent {
   title = "homes";
